c4ed5a6af939ce6397db2699fea56a3ef81df474
Hyeyoung <q4110879@gmail.com>
2023-12-27 15:02:57 +0900

    update

--- HW3/Student20200942.py
+++ HW3/Student20200942.py
@@ -4,53 +4,57 @@ import numpy as np
 import operator
 
 def classify0(inX, dataSet, labels, k):
-    dataSetSize = dataSet.shape[0] #dataSet의 갯수 세기
+    dataSetSize = dataSet.shape[0]
     diffMat = np.tile(inX, (dataSetSize, 1)) - dataSet
     sqDiffMat = diffMat ** 2
     sqDistances = sqDiffMat.sum(axis = 1)
     distances = sqDistances ** 0.5
-    sortedDistIndicies = distances.argsort() #간접정렬 숫자가 작은 순으로 인덱스값 리턴 
+    sortedDistIndicies = distances.argsort()
     classCount = {}
     for i in range(k):
-        voteIlabel = labels[sortedDistIndicies[i]] #작은 순서대로 라벨 값 얻음
-        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1 #라벨 갯수 하나씩 더함
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
     sortedClassCount = sorted(classCount.items(), key = operator.itemgetter(1), reverse = True)
-    return sortedClassCount[0][0] #더 많은 값 리턴(라벨) 많은 순서대로 리턴했으니까
+    return sortedClassCount[0][0]
+
+def makevector(filename) :
+	data = np.zeros((1, 1024))
+	f = open(filename)
+	for i in range(32):
+		line = f.readline()
+		for j in range(32):
+			data[0, 32 * i + j] = int(line[j])
+	return data
 
 def trainingNumber(folder):
     file_list = os.listdir(folder)
-    group = []
+    group = np.zeros((len(file_list), 1024))
     labels = []
+    i = 0
     for file_name in file_list:
         file_path = os.path.join(folder, file_name)
-        training_number = file_name.split("_")[0] 
-        labels.append(training_number) #무슨 숫자 training 했는지
-        with open(file_path, 'r') as f:
-            data = f.read() #데이터 읽기
-            data_list = [float(num) for num in data.split()]
-            group.append(data_list)
+        training_number = file_name.split("_")[0]
+        labels.append(training_number)
+        data = makevector(file_path)
+        group[i, :] = data
+        i += 1
+        
+    return group, labels
 
-    return np.array(group), labels
-
-def predictNumber(folder, training_data, training_labels, k) :
+def predictNumber(folder, training_data, training_labels, k):
     error = 0
-    entire = 0
 
     file_list = os.listdir(folder)
     for file_name in file_list:
-        answer = file_name.split("_")[0] #정답 숫자
+        answer = file_name.split("_")[0]
         file_path = os.path.join(folder, file_name)
-        with open(file_path, 'r') as f:
-            data = f.read() #데이터 읽기
-            data_list = [float(num) for num in data.split()]
-            p = classify0(data_list, training_data, training_labels, k)
-            if p != answer :
-                error += 1
-            entire += 1
-    rate = error / entire * 100
+        data = makevector(file_path)
+        p = classify0(data, training_data, training_labels, k)
+        if p != answer:
+            error += 1
+    rate = error / len(file_list) * 100
+    rate = int(rate)
     print("K={}일 때는 {}%".format(k, rate))
-
-
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -61,5 +65,5 @@ if __name__ == "__main__":
     data_folder = sys.argv[2]
 
     group, labels = trainingNumber(training_folder)
-    for i in range(1, 30) :
+    for i in range(1, 20) :
         predictNumber(data_folder, group, labels, i)
